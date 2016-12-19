383a3fc7331a3ad6a3094458f4cee9e7d44d820c
Hellseher <sharlatanus@gmail.com>
2016-12-19 22:08:04 +0000

    [E] Clean up and refactoring

--- init-local.el
+++ init-local.el
@@ -1,6 +1,6 @@
 ;;; init-local.el -- Custom local variables and functios.
 ;;; Created       : Thu 11 Aug 2016 22:32:01
-;;; Last Modified : <2016-12-11 Sun 23:04:03 GMT> sharlatan
+;;; Last Modified : <2016-12-17 Sat 23:19:18 GMT> sharlatan
 ;;; Author        : Sharlatan <sharlatanus@gmail.com>
 ;;; Maintainer(s) : Sharlatan
 ;;; Commentary:
@@ -16,64 +16,6 @@
       "8/Last Modified[ \t]*:\\\\?[ \t]*<%04Y-%:m-%02d %03a %02H:%02M:%02S %Z> %u\\\\?$" )
 
 (add-hook 'before-save-hook 'time-stamp)
-
-(require-package 'midnight)
-
-
-;; http://ergoemacs.org/emacs/modernization_upcase-word.html
-(defun toggle-letter-case ()
-  "Toggle the letter case of current word or text selection.
-Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
-  (interactive)
-  (let (p1 p2 (deactivate-mark nil) (case-fold-search nil))
-    (if (region-active-p)
-        (setq p1 (region-beginning) p2 (region-end))
-      (let ((bds (bounds-of-thing-at-point 'word) ) )
-        (setq p1 (car bds) p2 (cdr bds)) ) )
-
-    (when (not (eq last-command this-command))
-      (save-excursion
-        (goto-char p1)
-        (cond
-         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
-         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps") )
-         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps") )
-         ((looking-at "[[:lower:]]") (put this-command 'state "all lower"))
-         ((looking-at "[[:upper:]]") (put this-command 'state "all caps") )
-         (t (put this-command 'state "all lower") ) ) )
-      )
-
-    (cond
-     ((string= "all lower" (get this-command 'state))
-      (upcase-initials-region p1 p2) (put this-command 'state "init caps"))
-     ((string= "init caps" (get this-command 'state))
-      (upcase-region p1 p2) (put this-command 'state "all caps"))
-     ((string= "all caps" (get this-command 'state))
-      (downcase-region p1 p2) (put this-command 'state "all lower")) )
-    )
-  )
-
-;;set this to C-x M-c
-(global-set-key (kbd "C-x M-c") 'toggle-letter-case)
-
-(defun swap-windows ()
-  "If you have 2 windows, it swaps them."
-  (interactive)
-  (cond ((/= (count-windows) 2)
-         (message "You need exactly 2 windows to do this."))
-        (t
-         (let* ((w1 (first (window-list)))
-                (w2 (second (window-list)))
-                (b1 (window-buffer w1))
-                (b2 (window-buffer w2))
-                (s1 (window-start w1))
-                (s2 (window-start w2)))
-           (set-window-buffer w1 b2)
-           (set-window-buffer w2 b1)
-           (set-window-start w1 s2)
-           (set-window-start w2 s1))))
-  (other-window 1))
-(global-set-key (kbd "C-c s") 'swap-windows)
 
 ;;; Evil-mode
 (setq evil-toggle-key "C-'")
@@ -118,7 +60,15 @@ Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
 ;; https://www.emacswiki.org/emacs/download/multi-term.el
 
 (require-package 'multi-term)
-(global-set-key (kbd "C-x t") 'multi-term)
+(add-hook 'term-mode-hook
+          (lambda ()
+            (yas-minor-mode -1)
+            (add-to-list 'term-bind-key-alist '("C-c C-n" . multi-term-next))
+            (add-to-list 'term-bind-key-alist '("C-c C-p" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+            (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+            ))
+(global-set-key (kbd "C-c T") 'multi-term)
 
 
 ;;; Custom functios
@@ -155,7 +105,6 @@ Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
     (insert "][")
     (yank)
     (insert "]]")))
-
 
 (defun exzellenz/org-todo-move-to-top ()
   "Move TODO entery to the top ot the file when it is DONE."
